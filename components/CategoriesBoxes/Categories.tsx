dc9f6d87bb5a3facbe126d4aec5ca016bc70da6a
svoby <svobymates@gmail.com>
2024-08-28 12:13:21 +0200

    CategoriesBoxes different resizing layout

--- components/CategoriesBoxes/Categories.tsx
+++ components/CategoriesBoxes/Categories.tsx
@@ -2,7 +2,7 @@ import Category from "./Category";
 
 export default function Categories() {
   return (
-    <ul className="margins max flex  flex-wrap justify-center gap-[68px] gap-x-[10px] pt-[52px] text-black flex-row tab:justify-between items-center">
+    <ul className="margins max flex flex-col flex-wrap items-center justify-center gap-[68px] gap-x-[10px] pt-[52px] text-black sm:flex-row tab:justify-between">
       <Category category="headphones" />
       <Category category="speakers" />
       <Category category="earphones" />
