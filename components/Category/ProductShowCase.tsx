e95bc136b8a1354c68354d7ecbd20bc9bca92079
svoby <svobymates@gmail.com>
2024-10-20 13:05:18 +0200

    Category Image Priority

--- components/Category/ProductShowCase.tsx
+++ components/Category/ProductShowCase.tsx
@@ -4,14 +4,15 @@ import ShowCaseText from "./ProductShowCase/ShowCaseText";
 type Props = {
   slug: string;
   isOdd: boolean;
+  isFirst: boolean;
 };
 
-export default function ProductShowCase({ slug, isOdd }: Props) {
+export default function ProductShowCase({ slug, isOdd, isFirst }: Props) {
   return (
     <div
       className={`flex items-center justify-between gap-8 text-center sm:gap-[52px] lg:gap-5 lg:text-left ${isOdd && "lg:flex-row-reverse"} flex-col lg:flex-row`}
     >
-      <ShowCaseImages slug={slug} />
+      <ShowCaseImages slug={slug} isFirst={isFirst} />
       <ShowCaseText slug={slug} />
     </div>
   );
